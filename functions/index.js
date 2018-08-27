3566a995db632f3aed3d877812050a5d3cc01501
James Hrisho <james.hrisho@gmail.com>
2018-08-26 20:19:17 -0400

    Upgrade

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,12 @@
 
 process.env.DEBUG = 'actions-on-google:*';
 
-const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
+const { actionssdk } = require("actions-on-google");
 const functions = require('firebase-functions');
 const axios = require('axios')
 const statusUrl = functions.config().dryer.url
-
+// Create an app instance
+const app = actionssdk()
 const http = require('http');
 
 var instance = axios.create({
@@ -19,31 +20,27 @@ const requestConfig = {
   httpAgent: new http.Agent({ keepAlive: true }),
 };
 
-exports.sayDryer = functions.https.onRequest((request, response) => {
-  const app = new ActionsSdkApp({ request, response });
+// Register handlers for Actions SDK intents
 
-  function responseHandler(app) {
-    // intent contains the name of the intent you defined in `initialTriggers`
-    let intent = app.getIntent();
-    switch (intent) {
-      case app.StandardIntents.MAIN:
-        app.ask('Welcome! ask me about that dryer of yours.');
-        break;
-      default:
-        instance.request(requestConfig)
-          .then((resp) => {
-            const isVibrating = resp.data.isVibrating
-            const text = isVibrating ? "I'm shaking baby" : "I'm not running at all"
-            app.tell(text);
-          })
-          .catch((err) => {
-            console.error(err)
-            app.tell('oh no there was an error')
-          })
+app.intent('actions.intent.MAIN', conv => {
 
-        break;
-    }
+  return instance.request(requestConfig)
+    .then((resp) => {
+      const isVibrating = resp.data.isVibrating
+      const text = isVibrating ? "I'm shaking baby" : "I'm not running at all"
+      conv.close(text);
+    })
+    .catch((err) => {
+      console.error(err)
+      conv.close("oh no there was an error");
+    })
+})
+
+app.intent('actions.intent.TEXT', (conv, input) => {
+  if (input === 'bye' || input === 'goodbye') {
+    return conv.close('See you later!')
   }
-  // you can add the function name instead of an action map
-  app.handleRequest(responseHandler);
-});
+  conv.ask(`I didn't understand. Can you tell me something else?`)
+})
+
+exports.sayDryer = functions.https.onRequest(app);
